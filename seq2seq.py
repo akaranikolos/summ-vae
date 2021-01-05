937d9080e4d5730e96afecbcdef6923ecfb0c8ac
apostolos <akarani.neo1@gmail.com>
2021-01-05 17:18:46 +0200

    Update seq2seq.py

--- seq2seq.py
+++ seq2seq.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 import torch.optim as optim
 from torchtext.datasets import Multi30k
 from torchtext.data import Field, BucketIterator
-import numpy a snp
+import numpy as np
 import spacy
 import random
 from utils.tensorboard import SummaryWriter
