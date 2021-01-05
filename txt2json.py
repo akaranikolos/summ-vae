7c7070e9ed60d9d04ee78aab2921c7fcb43c4a72
apostolos <akarani.neo1@gmail.com>
2021-01-05 17:46:19 +0200

    update encoder

---
+++ txt2json.py
@@ -0,0 +1,13 @@
+import json
+for name in ['train', 'dev', 'test']:
+    with open(f'./data/{name}.source', 'r') as f:
+        src = f.readlines()
+    with open(f'./data/{name}.target', 'r') as f:
+        trg = f.readlines()
+    with open(f'./data/{name}.json', 'w') as f:
+        for s, t in zip(src, trg):
+            example = {'src': s, 'trg': t}
+            json.dump(example, f)
+            f.write('\n')
+
+            
