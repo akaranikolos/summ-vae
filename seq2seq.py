0c624402980537ea2d26b21f99a5cca2a7d68f78
Apostolos Karanikolos <a.karanikolos@uom.edu.gr>
2021-01-06 11:05:21 +0200

    functional as F

--- seq2seq.py
+++ seq2seq.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
+import torch.nn.functional as F
 from torchtext.data import TabularDataset
 from torchtext.data import Field, BucketIterator
 import numpy as np
