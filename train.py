4b0c3ae4aee10d2c835e7e9a4202127445e240c2
Apostolos Karanikolos <a.karanikolos@uom.edu.gr>
2021-01-06 10:42:43 +0200

    indentation

--- train.py
+++ train.py
@@ -43,37 +43,37 @@ def evaluate(model, iterator, criterion):
 
 def main(args):
     input_dim = len(SRC.vocab)
-	output_dim = len(TRG.vocab)
-	enc_emb_dim = args.embedding_size
-	dec_emb_dim = args.embedding_size
-	hid_dim = args.hidden_size
-	enc_dropout = args.embedding_dropout
-	dec_dropout = args.embedding_dropout
-	sos_idx = TRG.vocab.stoi['<sos>']
+    output_dim = len(TRG.vocab)
+    enc_emb_dim = args.embedding_size
+    dec_emb_dim = args.embedding_size
+    hid_dim = args.hidden_size
+    enc_dropout = args.embedding_dropout
+    dec_dropout = args.embedding_dropout
+    sos_idx = TRG.vocab.stoi['<sos>']
 
-	attn = Attention(hid_dim)
-	enc = Encoder(input_dim, enc_emb_dim, hid_dim, enc_dropout)
-	dec = Decoder(output_dim, dec_emb_dim, hid_dim, dec_dropout, attn)
-	model = Seq2Seq(enc, dec, sos_idx, device).to(device)
+    attn = Attention(hid_dim)
+    enc = Encoder(input_dim, enc_emb_dim, hid_dim, enc_dropout)
+    dec = Decoder(output_dim, dec_emb_dim, hid_dim, dec_dropout, attn)
+    model = Seq2Seq(enc, dec, sos_idx, device).to(device)
     print(f'The model has {count_parameters(model):,} trainable parameters')
     
-	optimizer = optim.Adam(model.parameters(), lr=args.learning_rate)
-	pad_idx = TRG.vocab.stoi['<pad>']
-	criterion = nn.CrossEntropyLoss(ignore_index=pad_idx)
-	n_epochs = args.epochs
-	clip = 1
-	save_dir = args.save_model_path
-	model_file_path = os.path.join(save_dir, 'model.pt')
-	best_valid_loss = float('inf')
-	if not os.path.isdir(f'{save_dir}'):
-    	os.makedirs(f'{save_dir}')
-	for epoch in range(n_epochs):    
-    	train_loss = train(model, train_iterator, optimizer, criterion, clip)
-    	valid_loss = evaluate(model, valid_iterator, criterion)    
-    	if valid_loss < best_valid_loss:
-        	best_valid_loss = valid_loss
-        	torch.save(model.state_dict(), model_file_path)    
-    	print(f'| Epoch: {epoch+1:03} | Train Loss: {train_loss:.3f} | Train PPL: {math.exp(train_loss):7.3f} | Val. Loss: {valid_loss:.3f} | Val. PPL: {math.exp(valid_loss):7.3f}') 
+    optimizer = optim.Adam(model.parameters(), lr=args.learning_rate)
+    pad_idx = TRG.vocab.stoi['<pad>']
+    criterion = nn.CrossEntropyLoss(ignore_index=pad_idx)
+    n_epochs = args.epochs
+    clip = 1
+    save_dir = args.save_model_path
+    model_file_path = os.path.join(save_dir, 'model.pt')
+    best_valid_loss = float('inf')
+    if not os.path.isdir(f'{save_dir}'):
+        os.makedirs(f'{save_dir}')
+    for epoch in range(n_epochs):    
+        train_loss = train(model, train_iterator, optimizer, criterion, clip)
+        valid_loss = evaluate(model, valid_iterator, criterion)    
+        if valid_loss < best_valid_loss:
+            best_valid_loss = valid_loss
+            torch.save(model.state_dict(), model_file_path)    
+        print(f'| Epoch: {epoch+1:03} | Train Loss: {train_loss:.3f} | Train PPL: {math.exp(train_loss):7.3f} | Val. Loss: {valid_loss:.3f} | Val. PPL: {math.exp(valid_loss):7.3f}') 
 
 
 if __name__ == '__main__':
