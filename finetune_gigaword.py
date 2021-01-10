4cd91988eb1ee1ae32a3fa903ab975eb74ab971a
apostolos <akarani.neo1@gmail.com>
2021-01-10 14:46:34 +0200

    update

---
+++ finetune_gigaword.py
@@ -0,0 +1,144 @@
+from transformers import BertTokenizerFast
+from datasets import load_from_disk, load_metric
+
+gigaword = load_from_disk("dataset/gigaword")
+train_data = gigaword['train']
+valid_data = gigaword['validation']
+test_data = gigaword['test']
+
+tokenizer = BertTokenizerFast.from_pretrained("bert-base-uncased")
+
+def map_to_length(x):
+  x["article_len"] = len(tokenizer(x["document"]).input_ids)
+  x["article_longer_512"] = int(x["article_len"] > tokenizer.model_max_length)
+  x["summary_len"] = len(tokenizer(x["summary"]).input_ids)
+  x["summary_longer_32"] = int(x["summary_len"] > 32)
+  x["summary_longer_64"] = int(x["summary_len"] > 64)
+  return x
+
+sample_size = 20000
+data_stats = train_data.select(range(sample_size)).map(map_to_length, num_proc=4)
+
+def compute_and_print_stats(x):
+  if len(x["article_len"]) == sample_size:
+    print(
+        "Article Mean: {}, %-Articles > 512:{}, Summary Mean:{}, %-Summary > 32:{}, %-Summary > 64:{}".format(
+            sum(x["article_len"]) / sample_size,
+            sum(x["article_longer_512"]) / sample_size, 
+            sum(x["summary_len"]) / sample_size,
+            sum(x["summary_longer_32"]) / sample_size,
+            sum(x["summary_longer_64"]) / sample_size,
+        )
+    )
+
+output = data_stats.map(
+  compute_and_print_stats, 
+  batched=True,
+  batch_size=-1,
+)
+
+encoder_max_length=128
+decoder_max_length=32
+
+def process_data_to_model_inputs(batch):
+  # tokenize the inputs and labels
+  inputs = tokenizer(batch["document"], padding="max_length", truncation=True, max_length=encoder_max_length)
+  outputs = tokenizer(batch["summary"], padding="max_length", truncation=True, max_length=decoder_max_length)
+
+  batch["input_ids"] = inputs.input_ids
+  batch["attention_mask"] = inputs.attention_mask
+  batch["decoder_input_ids"] = outputs.input_ids
+  batch["decoder_attention_mask"] = outputs.attention_mask
+  batch["labels"] = outputs.input_ids.copy()
+
+  # because BERT automatically shifts the labels, the labels correspond exactly to `decoder_input_ids`. 
+  # We have to make sure that the PAD token is ignored
+  batch["labels"] = [[-100 if token == tokenizer.pad_token_id else token for token in labels] for labels in batch["labels"]]
+
+  return batch
+
+batch_size = 16
+
+train_data = train_data.map(
+    process_data_to_model_inputs, 
+    batched=True, 
+    batch_size=batch_size, 
+    remove_columns=["document", "summary"]
+)
+train_data.set_format(type="torch", columns=["input_ids", "attention_mask", "decoder_input_ids", "decoder_attention_mask", "labels"])
+
+valid_data = valid_data.map(
+    process_data_to_model_inputs, 
+    batched=True, 
+    batch_size=batch_size, 
+    remove_columns=["document", "summary"]
+)
+val_data.set_format(type="torch", columns=["input_ids", "attention_mask", "decoder_input_ids", "decoder_attention_mask", "labels"])
+
+from transformers import EncoderDecoderModel
+
+# TODO experiment with other decoders
+bert2bert = EncoderDecoderModel.from_encoder_decoder_pretrained("bert-base-uncased", "bert-base-uncased")
+print(bert2bert)
+
+print(f"\nNum Params. Non-Shared: {bert2bert.num_parameters()}")
+
+bert2bert.config.decoder_start_token_id = tokenizer.cls_token_id
+bert2bert.config.eos_token_id = tokenizer.sep_token_id
+bert2bert.config.pad_token_id = tokenizer.pad_token_id
+bert2bert.config.vocab_size = bert2bert.config.encoder.vocab_size
+
+bert2bert.config.max_length = 40
+bert2bert.config.min_length = 9
+bert2bert.config.no_repeat_ngram_size = 3
+bert2bert.config.early_stopping = True
+bert2bert.config.length_penalty = 2.0
+bert2bert.config.num_beams = 4
+
+from seq2seq_trainer import Seq2SeqTrainer
+from seq2seq_training_args import Seq2SeqTrainingArguments
+
+training_args = Seq2SeqTrainingArguments(
+    predict_with_generate=True,
+    evaluation_strategy="steps",
+    per_device_train_batch_size=batch_size,
+    per_device_eval_batch_size=batch_size,
+    fp16=True, 
+    output_dir="./",
+    logging_steps=1000,
+    save_steps=500,
+    eval_steps=7500,
+    warmup_steps=2000,
+    save_total_limit=3,
+)
+
+rouge = datasets.load_metric("rouge")
+
+def compute_metrics(pred):
+    labels_ids = pred.label_ids
+    pred_ids = pred.predictions
+
+    pred_str = tokenizer.batch_decode(pred_ids, skip_special_tokens=True)
+    labels_ids[labels_ids == -100] = tokenizer.pad_token_id
+    label_str = tokenizer.batch_decode(labels_ids, skip_special_tokens=True)
+
+    rouge_output = rouge.compute(predictions=pred_str, references=label_str, rouge_types=["rouge2"])["rouge2"].mid
+
+    return {
+        "rouge2_precision": round(rouge_output.precision, 4),
+        "rouge2_recall": round(rouge_output.recall, 4),
+        "rouge2_fmeasure": round(rouge_output.fmeasure, 4),
+    }
+
+trainer = Seq2SeqTrainer(
+    model=bert2bert,
+    args=training_args,
+    compute_metrics=compute_metrics,
+    train_dataset=train_data,
+    eval_dataset=valid_data,
+)
+
+trainer.train()
+
+
+
--- load_gigaword.py
+++ load_gigaword.py
@@ -2,18 +2,19 @@ import datasets
 from datasets import load_dataset
 
 gigaword = load_dataset("gigaword")
+gigaword.save_to_disk("dataset/gigaword")
 
 print(gigaword)
 # train: 3,803,957  validation:189,651 test: 1,951 
 #[document, summary]
 
-print(sum(len(article) for article in gigaword['train']['document']) / len(gigaword['train']['document']))
+#print(sum(len(article) for article in gigaword['train']['document']) / len(gigaword['train']['document']))
  # 181.53 chars
-print(sum(len(article) for article in gigaword['train']['summary']) / len(gigaword['train']['summary']))
+#print(sum(len(article) for article in gigaword['train']['summary']) / len(gigaword['train']['summary']))
  # 51.06 chars
-print(sum(len(summary) for summary in gigaword['validation']['document']) / len(gigaword['validation']['document']))
+#print(sum(len(summary) for summary in gigaword['validation']['document']) / len(gigaword['validation']['document']))
  # 181.50 chars
-print(sum(len(summary) for summary in gigaword['validation']['summary']) / len(gigaword['validation']['summary']))
+#print(sum(len(summary) for summary in gigaword['validation']['summary']) / len(gigaword['validation']['summary']))
 # 51.81 chars
 
 
