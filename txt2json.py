474a29b6f86feac13d685c060c89c927a73fcc6c
apostolos <akarani.neo1@gmail.com>
2021-01-10 16:07:19 +0200

    Update txt2json.py

--- txt2json.py
+++ txt2json.py
@@ -1,10 +1,10 @@
 import json
-for name in ['train', 'dev', 'test']:
-    with open(f'./data/{name}.source', 'r') as f:
+for name in ['train', 'valid', 'test']:
+    with open(f'./data/cnndm/{name}.source', 'r') as f:
         src = f.readlines()
-    with open(f'./data/{name}.target', 'r') as f:
+    with open(f'./data/cnndm/{name}.target', 'r') as f:
         trg = f.readlines()
-    with open(f'./data/gigaword/{name}.json', 'w') as f:
+    with open(f'./data/cnndm/{name}.json', 'w') as f:
         for s, t in zip(src, trg):
             example = {'src': s, 'trg': t}
             json.dump(example, f)
